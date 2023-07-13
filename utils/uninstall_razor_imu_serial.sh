0862c2369057b3b268a70ef8af9a6c5942284e72
Kyle Thompson <kat0028@auburn.edu>
2023-07-13 10:07:54 -0500

    updated uninstall finished cmake files

--- utils/uninstall_razor_imu_serial.sh
+++ utils/uninstall_razor_imu_serial.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo rm -rf /usr/local/lib/razor_imu_serial
 sudo rm -rf /usr/local/include/razor_imu_serial
-sudo rm -rf /usr/lib/cmake/razor_imu_serial
+sudo rm -rf /usr/local/lib/cmake/razor_imu_serial
